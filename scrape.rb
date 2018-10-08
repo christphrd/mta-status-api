2d326e1587acd9c7dc2fb1f041c7ac70da1246a9
christphrd <diep.christopher@gmail.com>
2018-10-08 17:46:32 -0400

    clearer log to console

--- scrape.rb
+++ scrape.rb
@@ -6,6 +6,8 @@ def get_doc(index_url)
   @doc = Nokogiri::HTML(open(index_url))
   @subway_table = @doc.css('#subwayDiv')[0].css('table')
 
+  p "----------------------"
+
   @subway_table.search('tr').each do |tr|
     cells = tr.search('th, td')
 
@@ -15,6 +17,8 @@ def get_doc(index_url)
 
       #delay or not? delay would return an nokogiri::xml::element. not would be an empty array
       cells[1].css('.subway_delays').empty? ? (p "Not Delayed") : (p "Delays")
+
+      p "----------------------"
     end
 
   end
